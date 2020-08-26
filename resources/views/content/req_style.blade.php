d14194bf16e3e43c52941d958ba00e6f7e08871a
Mitch <calvin.ongkingco@gmail.com>
2020-08-26 12:25:07 +0200

    updated fonts

--- resources/views/content/req_style.blade.php
+++ resources/views/content/req_style.blade.php
@@ -4,3 +4,9 @@
 <link rel="stylesheet" href="{{asset(url('/public/css/docs.css'))}}">
 <link rel="stylesheet" href="{{asset(url('/public/css/flag-icon.css'))}}">
 <link rel="stylesheet" href="{{asset(url('/public/css/dgz.css'))}}">
+<style type="text/css">
+    @font-face {
+        font-family: OptimusPrinceps;
+        src: url('{{ public_path('fonts/MuseoSans-900.otf') }}');
+    }
+</style>
